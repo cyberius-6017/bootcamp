24a5034d807ba812351798dda3d3c749d484af7d
Fernando Mendizabal <weapon0013@gmail.com>
2019-12-17 12:18:29 -0600

    Controllers added
    Added more controllers

---
+++ RobotCode/Test/src/main/java/frc/robot/commands/MoveBack.java
@@ -0,0 +1,39 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class MoveBack extends Command{
+    public MoveBack(){
+        requires(Robot.driveTrain);
+    }
+
+    // Called just before this Command runs the first time
+    @Override
+    protected void initialize() {
+    }
+
+  // Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute() {
+    }
+
+
+    @Override
+    protected boolean isFinished() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+
+    // Called once after isFinished returns true
+     @Override
+     protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    @Override
+     protected void interrupted() {
+    }
+}
